62442c672df95e7f8e0ec3a20d9a24c8316c83ac
Felixx <phaindnse05704@fpt.edu.vn>
2018-06-21 10:27:21 +0700

    Fixed carousel

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -2,19 +2,21 @@
     <div :id="keyID" class="carousel slide" data-ride="carousel">
     <!-- Indicators -->
     <ul class="carousel-indicators">
-        <li :data-target="getId" v-for="(item, index) in list" :key="item"  :data-slide-to="index + 1" :class="{'active' : index == 0}"></li>
+        <li :data-target="getId" v-for="(item, index) in list" :key="item"  :data-slide-to="index" :class="{'active' : index == 0}"></li>
     </ul>
     <!-- The slideshow -->
     <div class="carousel-inner">
         <div class="carousel-item" v-for="(item, index) in list" :key="item" :class="{'active' : index == 0}">
-            <img :src="require('../assets/shop/'+item.img[0])" :alt="item.name">
+            <center>
+            <img :src="require('../assets/shop/'+item.img[0])" :alt="item.name" width="460" height="345">
+            </center>
         </div>
     </div>
     <!-- Left and right controls -->
-    <a class="carousel-control-prev" data-slide="prev">
+    <a class="carousel-control-prev" :href="getId()" data-slide="prev">
         <span class="carousel-control-prev-icon"></span>
     </a>
-    <a class="carousel-control-next" data-slide="next">
+    <a class="carousel-control-next" :href="getId()" data-slide="next">
         <span class="carousel-control-next-icon text-black-50"></span>
     </a>
     
@@ -37,4 +39,7 @@ export default {
 </script>
 
 <style scoped>
+a[data-slide] {
+  background: rgba(156, 153, 153, 0.5);
+}
 </style>
