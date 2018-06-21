9417292778035c042c4d9222ffbb41c5d930aa33
Felixx <phaindnse05704@fpt.edu.vn>
2018-06-21 09:52:02 +0700

    Fixed bind src attribute

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -1,38 +1,31 @@
 <template>
-    <!-- <div :id="keyID" class="carousel slide" data-ride="carousel"> -->
+    <div :id="keyID" class="carousel slide" data-ride="carousel">
     <!-- Indicators -->
-    <!-- <ul class="carousel-indicators"> -->
-        <!-- <li :data-target="getId" v-for="(item, index) in list" :key="item"  :data-slide-to="index" :class="{'active' : index == 0}"></li> -->
-    <!-- </ul> -->
+    <ul class="carousel-indicators">
+        <li :data-target="getId" v-for="(item, index) in list" :key="item"  :data-slide-to="index + 1" :class="{'active' : index == 0}"></li>
+    </ul>
     <!-- The slideshow -->
-    <!-- <div class="carousel-inner"> -->
-        <!-- <div class="carousel-item" v-for="(item, index) in list" :key="item" :class="{'active' : index == 1}"> -->
-            <!-- <img :src="'../assets/shop/'+item.img[0]" :alt="item.name"> -->
-        <!-- </div> -->
-    <!-- </div> -->
-    <!-- Left and right controls -->
-    <!-- <a class="carousel-control-prev" :href="getId" data-slide="prev"> -->
-        <!-- <span class="carousel-control-prev-icon"></span> -->
-    <!-- </a> -->
-    <!-- <a class="carousel-control-next" :href="getId" data-slide="next"> -->
-        <!-- <span class="carousel-control-next-icon"></span> -->
-    <!-- </a> -->
-    
-<!-- </div> -->
-    <div>
-        <img src="../assets/shop/ts01-w.jpg" alt="">
-        <div v-for="item in list" :key="item.id">
-            <p>{{ item.name }} + {{getImg(item.img[0])}}</p>
-            <img v-bind:src="getImg(item.img[0])" alt="">
+    <div class="carousel-inner">
+        <div class="carousel-item" v-for="(item, index) in list" :key="item" :class="{'active' : index == 0}">
+            <img :src="require('../assets/shop/'+item.img[0])" :alt="item.name">
         </div>
     </div>
+    <!-- Left and right controls -->
+    <a class="carousel-control-prev" data-slide="prev">
+        <span class="carousel-control-prev-icon"></span>
+    </a>
+    <a class="carousel-control-next" data-slide="next">
+        <span class="carousel-control-next-icon text-black-50"></span>
+    </a>
+    
+</div>
 </template>
 
 <script>
 export default {
   name: "Carousel",
   props: ["keyID", "list"],
-  computed: {
+  methods: {
     getId: function() {
       return "#" + this.keyID;
     },
